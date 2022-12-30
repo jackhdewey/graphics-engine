71431aa004168d304bc8c23788f94ec53842bf2c
jackhdewey <jackhdewey@gmail.com>
2022-12-30 09:14:57 -0500

    Updated comments

--- shaders/vert.glsl
+++ shaders/vert.glsl
@@ -1,21 +1,22 @@
 // ==================================================================
 #version 330 core
 
-// Read in attributes stored in vertex buffer object
+// ======================= IN =========================
 layout(location=0)in vec3 position; 
 layout(location=1)in vec2 texCoord;
 layout(location=2)in vec3 normal;
 layout(location=3)in vec3 tangent;
 layout(location=4)in vec3 bitangent;
 
+// ======================= UNIFORMS ====================
 // Transformation matrices
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 
-// Fragment position in world space (for computing lighting)
+// ======================= OUT ========================
+// Fragment position in world space
 out vec3 FragPos;
-
 // Texture coordinates and vertex normals
 out vec2 v_texCoord;
 out vec3 v_normal;
@@ -27,7 +28,6 @@ void main()
 
     // Transform each fragment into world space (for computing lighting)
     FragPos = vec3(model * vec4(position, 1.0f));
-
     // Pass normal and texture coordinates to the next stage
     v_texCoord = texCoord;
     v_normal = normal;
